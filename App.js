4e75a78a6eb0c1c67daaabb34dd40238c7125d69
Prashidha0O1 <89299485+Prashidha0O1@users.noreply.github.com>
2024-01-29 14:37:11 +0545

    Addded Files

---
+++ App.js
@@ -0,0 +1,16 @@
+// Get button element 
+function subscribeAlert() {
+    alert("Thank you for subscribing!"); 
+  }
+function toggleContent() {
+    var additionalContent = document.getElementById("additional-content");
+    var readMoreButton = document.getElementById("read-more");
+
+    if (additionalContent.style.display === "none") {
+        additionalContent.style.display = "block";
+        readMoreButton.innerHTML = "Read less";
+    } else {
+        additionalContent.style.display = "none";
+        readMoreButton.innerHTML = "Read more";
+    }
+}
